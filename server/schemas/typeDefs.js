2a46a8f1ff75036f658078140569a4fc4002c138
Your Name <you@example.com>
2023-01-03 15:14:33 -0600

    Adding some specifications for book search gql, added comments for future needs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,28 +1,35 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type Tech {
+  type User {
     _id: ID!
-    name: String!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: ??(This will be an array of the Book type.)
   }
 
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+  type Book {
+    bookId: ID!
+    authors: ??(An array of strings, as there may be more than one author)
+    description: String!
+    title: String
+    image: ????
+    link: String????
   }
 
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+    me: [User]
+    user(userId: ID!): User
   }
 
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
+   login:??? Accepts an email and password as parameters; returns an Auth type
+   addUser(Accepts a username, email, and password as parameters; returns an Auth type)
+   saveBook(book authors array, description, title, bookId,image, and link as parameters, look into input type)
+   removeBook(bookId) : User
   }
 `;
+//Auth type: token, user( References the User type.)
 
 module.exports = typeDefs;
