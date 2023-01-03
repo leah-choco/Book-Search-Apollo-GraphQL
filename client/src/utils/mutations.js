f3cec2cebbb97b0fa386d7244f0d15b071bb2a27
Your Name <you@example.com>
2023-01-03 15:42:05 -0600

    Setting up mutations.js and adding comments for future use

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client";
+
+//will execute the loginUser mutation set up using Apollo Server
+export const LOGIN_USER = gql``;
+
+//will execute the addUser mutation
+export const ADD_USER = gql``;
+
+//will execute the saveBook mutation
+export const SAVE_BOOK = gql``;
+
+//will execute the removeBook mutation
+export const REMOVE_BOOK = gql``;
