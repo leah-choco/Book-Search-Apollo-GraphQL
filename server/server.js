0a46c986db006c5ec399b030930b76a48575e5ff
Your Name <you@example.com>
2023-01-07 10:35:40 -0600

    Removing routes folder entirely and from the server.js file, as it is no longer needed for gql and Apollo

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const { authMiddleware } = require("./utils/auth");
 //Adding schemas
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-//const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
