d40ec02424a4afbdacd21e3d78fedf4b7ce233c3
Your Name <you@example.com>
2023-01-10 13:06:40 -0600

    Working through defining useMutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -26,7 +26,10 @@ const SearchBooks = () => {
   const [searchInput, setSearchInput] = useState("");
 
   // create state to hold saved bookId values
-  const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [saveBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  //Defining the saveBook mutation
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -81,7 +84,7 @@ const SearchBooks = () => {
 
     try {
       const { data } = await saveBook({
-        variables: { bookData: bookToSave },
+        variables: { newBook: { ...bookToSave } },
       });
 
       // if book successfully saves to user's account, save book id to state
